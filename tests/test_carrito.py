ba3ad3075e927a8e2a43b49e6fab3b5fef9ecc6b
FlorCordisco <florcordisco@gmail.com>
2025-11-04 15:13:14 -0300

    ultimos cambios para pre entrega

--- tests/test_carrito.py
+++ tests/test_carrito.py
@@ -8,12 +8,14 @@ def test_carrito(login_in_driver):
         productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
         # Añadir el primer producto al carrito
         productos[0].find_element(By.TAG_NAME, 'button').click()
+
         # Confirmar que el badge del carrito muestra 1
         badge = driver.find_element(By.CLASS_NAME,'shopping_cart_badge').text
+       
         assert badge == '1'
         print('Carrito OK →', badge)
     except Exception as e:
-        print(f"Error en test_navegacion: {e}")
+        print(f"Error en test_carrito: {e}")
         raise
     finally:
         driver.quit()
