088e9f46dc033f2ba7d240370c4ea8248fbe2799
FlorCordisco <florcordisco@gmail.com>
2025-11-26 19:30:53 -0300

    ultimos cambios y nuevos archivos para tests y pages

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
 from pages.login_page import LoginPage
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--incognito")
+
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
--- run_tests.py
+++ run_tests.py
@@ -3,11 +3,12 @@ import pytest
 # Lista de archivos de pruebas a ejecutar
 test_files = [
     "tests/test_login.py",
-    "tests/test_navegacion.py",
-    "tests/test_carrito.py"
+    "tests/test_inventory.py",
+    "tests/test_cart.py",
+    "tests/test_cart_json.py"
 ]
 
-# Argumentod para ejecutar las pruebas: archivos + reporte HTML
+# Argumentos para ejecutar las pruebas: archivos + reporte HTML
 pytest_args = test_files + ["--html=report.html","--self-contained-html","-v"]
 
 pytest.main(pytest_args)
