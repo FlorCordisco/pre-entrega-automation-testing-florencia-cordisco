6b9f1feb30ca39e8644b3d8b01cc48d3e1e8c0e7
FlorCordisco <florcordisco@gmail.com>
2025-12-05 10:09:53 -0300

    Test para api terminado

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,8 @@ test_files = [
     "tests/test_login.py",
     "tests/test_inventory.py",
     "tests/test_cart.py",
-    "tests/test_cart_json.py"
+    "tests/test_cart_json.py",
+    "tests/test_api_reqres.py"
 ]
 
 # Argumentos para ejecutar las pruebas: archivos + reporte HTML
